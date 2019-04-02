79780881b0d13e7c7f9d58a2777b4210347581d7
Debugg3R <debugg3r@gmail.com>
2019-04-02 22:58:48 +0300

    scroll fixed

--- app/src/main/java/com/debugg3r/android/academy01/MainActivity.java
+++ app/src/main/java/com/debugg3r/android/academy01/MainActivity.java
@@ -2,6 +2,8 @@ package com.debugg3r.android.academy01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView view = findViewById(R.id.text_vew_description);
+        view.setMovementMethod(new ScrollingMovementMethod());
     }
 }
